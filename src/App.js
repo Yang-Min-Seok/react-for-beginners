083ae4fc77a9473645e5f4e2c8f06fcaee81d74c
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-10 18:24:13 +0900

    #6.0 Introduction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
+
 function App() {
+  
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('call an api')
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={'continue'}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }

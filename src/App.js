a978f5185b51327934c30d4d9523b1ec3e7abaf1
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-11 15:43:57 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+
+  // useEffect runs function
+  // and reapeat it every time the dependencies(inside []) changes
+  useEffect(() => {
+    console.log("Hi :)");
+    return () => {
+      console.log("bye :(");
+    }
+  }, [])
+  
+  // same meaning another ver.
+  // useEffect runs function
+  // and reapeat it every time the dependencies(inside []) changes
+  // useEffect(function () {
+  //   console.log("Hi :)");
+  //   return function () {
+  //     console.log("bye :(");
+  //   }
+  // }, []);
+
+  return <h1>Hello</h1>
+}
+
 function App() {
   
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('I run all the time');
-
-  // useEffect helps us to call a code only one time
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  
-  // This code runs every time the keyword changes
-  useEffect(() => {
-    // if the keyword is longer than 5 -> working
-    if (keyword.length > 5) {
-      console.log(`I run when ${keyword} changes`);
-    }
-  }, [keyword]);
-  
-  // This code runs every time the counter changes
-  useEffect(() => {
-    console.log(`I run when ${counter} chagnes`);
-  }, [counter])
-
-  // This code runs every time either the counter or keyword changes
-  useEffect(() => {
-    console.log('I run when counter or keyword changes');
-  },[counter, keyword])
-
+  // showing -> false(default)
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(curr => !curr);
   return (
     <div>
-      <input 
-      value={keyword} 
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here...">
-      </input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

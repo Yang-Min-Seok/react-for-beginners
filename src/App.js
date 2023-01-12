c8351f456c564ccd941629ba870c6eabb505eb13
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-13 00:28:50 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,6 @@
-import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([])
-  
-  // How to call API modernly
-  const getMovies = async() => {
-    const json = await (
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    // update movies to json.data.movies
-    setMovies(json.data.movies);
-    // update current loading status to false
-    setLoading(false);
-  };
-  
-  // Call only once
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-  <div>
-    {loading ? <h1>Loading...</h1> : 
-      <div>
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image}></img>
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    }
-  </div>
-  );
+  return null;
 }
 
 export default App; 
---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import Movie from "../components/Movie"
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([])
+    
+    // How to call API modernly
+    const getMovies = async() => {
+        const json = await (
+        await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        )
+        ).json();
+        // update movies to json.data.movies
+        setMovies(json.data.movies);
+        // update current loading status to false
+        setLoading(false);
+    };
+    
+    // Call only once
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+    <div>
+        {loading ? <h1>Loading...</h1> : 
+        <div>
+            {movies.map((movie) => (
+            <Movie 
+            key={movie.id}
+            coverImg={movie.medium_cover_image} 
+            title={movie.title} 
+            summary={movie.summary} 
+            genres={movie.genres} />
+            ))}
+        </div>
+        }
+    </div>
+    );
+}
+export default Home;

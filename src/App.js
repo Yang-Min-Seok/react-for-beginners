d862330c6470e223d943f34c3c4c28a570f32cde
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-12 22:29:59 +0900

    Homework
    * make own circulator about buying coins :)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  // 
+  
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [coinPrice, setCoinPrice] = useState(0);
+  const [coinName, setCoinName] = useState("");
 
   // calling Api (only once)
   useEffect(() => {
@@ -16,18 +19,36 @@ function App() {
       });
   }, []);
   
+  function onChange (event) {
+    setMoney(event.target.value);
+  }
+
+  function getPriceAndName (event) {
+    const idx = event.target.selectedIndex;
+    setCoinPrice(coins[idx].quotes.USD.price);
+    setCoinName(coins[idx].id);
+  }
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? <strong> Loading... </strong> : (
-      <select>
-        {coins.map((coin) => (
+      <select
+      onChange={getPriceAndName}
+      >
+        {coins.map((coin, idx) => (
         <option>
           {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD 
         </option>
         ))}
       </select>)}
-      
+      <input 
+        type="number"
+        value={money}
+        onChange={onChange}
+      >
+      </input>
+      <p> {money && coinPrice ? `U can buy ${Math.round(money / coinPrice)} coins of ${coinName}` : "Please select the coin you want to buy and the budget"} </p>
     </div>
   )
 }

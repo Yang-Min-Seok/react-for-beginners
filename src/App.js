5715e5ce522b46f4dcb16547fab9d6154312b0b6
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-12 17:33:47 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  // 
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    // stop refreshing
-    event.preventDefault();
-    // if toDo is empty
-    if (toDo === "") {
-      // kill the function
-      return;
-    };
-    // currArrray would be argument
-    setToDos(currArray => [toDo, ...currArray]);
-    // After summiting, make toDo empty
-    setToDo("");
-  };
-
+  // calling Api (only once)
+  useEffect(() => {
+    // How to call Api by address
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-        onChange={onChange} 
-        value={toDo} 
-        type="text" 
-        placeholder="Write your to do,,," 
-        />
-        <button>Add To Do </button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((curr_todo, index) => (
-          <li key={index} >{curr_todo}</li>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong> Loading... </strong> : (
+      <select>
+        {coins.map((coin) => (
+        <option>
+          {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD 
+        </option>
         ))}
-      </ul>
+      </select>)}
+      
     </div>
   )
 }

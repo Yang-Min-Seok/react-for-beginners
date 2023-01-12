796d230267f9485a04b9738e73c6dfe252d18e6d
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-13 00:02:34 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [coinPrice, setCoinPrice] = useState(0);
-  const [coinName, setCoinName] = useState("");
-
-  // calling Api (only once)
-  useEffect(() => {
-    // How to call Api by address
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
+  const [movies, setMovies] = useState([])
   
-  function onChange (event) {
-    setMoney(event.target.value);
-  }
-
-  function getPriceAndName (event) {
-    const idx = event.target.selectedIndex;
-    setCoinPrice(coins[idx].quotes.USD.price);
-    setCoinName(coins[idx].id);
-  }
+  // How to call API modernly
+  const getMovies = async() => {
+    const json = await (
+      await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    // update movies to json.data.movies
+    setMovies(json.data.movies);
+    // update current loading status to false
+    setLoading(false);
+  };
+  
+  // Call only once
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong> Loading... </strong> : (
-      <select
-      onChange={getPriceAndName}
-      >
-        {coins.map((coin) => (
-        <option>
-          {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD 
-        </option>
+  <div>
+    {loading ? <h1>Loading...</h1> : 
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}></img>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>
         ))}
-      </select>)}
-      <input 
-        type="number"
-        value={money}
-        onChange={onChange}
-      >
-      </input>
-      <p> {money && coinPrice ? `U can buy ${Math.round(money / coinPrice)} coins of ${coinName}` : "Please select the coin you want to buy and the budget"} </p>
-    </div>
-  )
+      </div>
+    }
+  </div>
+  );
 }
 
-export default App;
+export default App; 

46a07c4501806c3df9685c5f7f7207ecb2d49a6e
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-14 14:28:12 +0900

    #7.9 Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,7 @@ function Movie({id, coverImg, title, summary, genres}) {
       <h2>
        <Link to ={`/movie/${id}`}> {title} </Link> 
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary }</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
